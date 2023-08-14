9f99963d46e8b6e5b6c5196c7623066e1fb7f5b5
Lloyd Stevens <stevens.lloyd@gmail.com>
2023-08-14 13:02:19 +0100

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <h1>Lloyd Stevens</h1>
+        </header>
+        {children}
+        <footer>
+          <p>&copy; Lloyd Stevens 2023</p>
+        </footer>
+        </body>
     </html>
   )
 }
